add2c119cd7e968450a1d082100628330a058a6e
Eric <eric@canva.com>
2019-03-11 00:15:48 +1100

    fix

--- src/sort/merge_sort.rs
+++ src/sort/merge_sort.rs
@@ -1,8 +1,5 @@
 // beg...split | split..end + 1
 fn merge(arr: &mut [i32], beg: usize, end: usize, split: usize) {
-    if beg == split || split == end {
-        return;
-    }
     let mut merge_result = Vec::with_capacity(end - beg + 1);
     let mut i = beg;
     let mut j = split;
@@ -29,18 +26,11 @@ fn merge(arr: &mut [i32], beg: usize, end: usize, split: usize) {
 fn split(arr: &mut [i32], beg: usize, end: usize) {
     match end - beg {
         0 => (),
-        1 if arr[beg] > arr[end] => {
-            arr.swap(beg, end);
-            merge(arr, beg, end, end);
-        },
-        1 => (),
         _ => {
             let mid = (beg + end) / 2;
             split(arr, beg, mid);
             split(arr, mid + 1, end);
-            println!("{:?}, {}, {}, {}", arr, beg, mid, end);
             merge(arr, beg, end, mid + 1);
-            println!("{:?}", arr);
         }
     }
 }
@@ -59,9 +49,5 @@ mod test {
 //        let mut case0 = [1, 3, 4, 5, 2, 5, 10, 12];
 //        super::merge(&mut case0, 0, 7, 4);
 //        assert_eq!(case0, [1, 2, 3, 4, 5, 5, 10, 12]);
-
-        let mut case1 = [1, 3, 3, 6, 6, -1, 10, 4, -5, -19];
-        super::merge(&mut case1, 5, 6, 7);
-        assert_eq!(case1, [1, 3, 3, 6, 6, -1, 4, 10, -5, -19])
     }
 }
