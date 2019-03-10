fb0ae4fe66be8414fd9cccd91a440a0b38990056
Eric Wang <eric@canva.com>
2019-03-11 00:19:34 +1100

    Merge sort (#2)

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 mod sort;
 
 fn main() {
-    let mut arr = ['a', 's', 'b'];
-    sort::insert_sort(&mut arr);
+    let mut arr = [3, 1, 6, 3, 6, 10, -1, 4];
+    sort::merge_sort(&mut arr);
     println!("{:?}", arr);
 }
---
+++ src/sort/merge_sort.rs
@@ -0,0 +1,53 @@
+// beg...split | split..end + 1
+fn merge(arr: &mut [i32], beg: usize, end: usize, split: usize) {
+    let mut merge_result = Vec::with_capacity(end - beg + 1);
+    let mut i = beg;
+    let mut j = split;
+    while i < split || j < end + 1 {
+        if j >= end + 1 {
+            merge_result.push(arr[i]);
+            i += 1;
+        } else if i >= split {
+            merge_result.push(arr[j]);
+            j += 1;
+        } else if arr[i] > arr[j] {
+            merge_result.push(arr[j]);
+            j += 1;
+        } else {
+            merge_result.push(arr[i]);
+            i += 1;
+        }
+    }
+    for idx in 0..merge_result.len() {
+        arr[beg + idx] = merge_result[idx];
+    }
+}
+
+fn split(arr: &mut [i32], beg: usize, end: usize) {
+    match end - beg {
+        0 => (),
+        _ => {
+            let mid = (beg + end) / 2;
+            split(arr, beg, mid);
+            split(arr, mid + 1, end);
+            merge(arr, beg, end, mid + 1);
+        }
+    }
+}
+
+pub fn sort(arr: &mut [i32]) {
+    match arr.len() {
+        0 | 1 => return,
+        _ => split(arr, 0, arr.len() - 1)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn merge() {
+        let mut case0 = [1, 3, 4, 5, 2, 5, 10, 12];
+        super::merge(&mut case0, 0, 7, 4);
+        assert_eq!(case0, [1, 2, 3, 4, 5, 5, 10, 12]);
+    }
+}
