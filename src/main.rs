55373b7b8e829616c9623f9085e71e5cfa4d6b9b
Eric Wang <wjun0912@gmail.com>
2019-05-06 20:55:10 +1000

    max_subarray:brute_force (#3)

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 mod sort;
+mod max_subarray;
 
 fn main() {
-    let mut arr = [3, 1, 6, 3, 6, 10, -1, 4];
-    sort::merge_sort(&mut arr);
-    println!("{:?}", arr);
 }
---
+++ src/max_subarray/mod.rs
@@ -0,0 +1,28 @@
+pub fn by_brute(arr: & [i32]) -> i32 {
+    let len = arr.len();
+    if len < 2 { return 0 }
+    let mut max_diff = 0;
+    let mut diff: i32;
+    for i in 0..(len - 1) {
+        for j in (i + 1)..len {
+            diff = arr[j] - arr[i];
+            if diff > max_diff { max_diff = diff; }
+        }
+    }
+    max_diff
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn empty() {
+        assert_eq!(super::by_brute(&[]), 0);
+        assert_eq!(super::by_brute(&[1]), 0);
+    }
+    #[test]
+    fn simple() {
+        assert_eq!(super::by_brute(&[-5, -1, 2, 5, 1, 0, 10, -20, 5, 12, 5, 4, 9]), 32);
+        assert_eq!(super::by_brute(&[-5, -1, 2, 5, 1, 0, 10, -20, 5, 1, 5, 4, 9]), 29);
+        assert_eq!(super::by_brute(&[-5, -1, 2, 5, 1, 0, 10, -2, 5, 1, 5, 4, 9]), 15);
+    }
+}
