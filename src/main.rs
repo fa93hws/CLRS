0469947f0b2402474f31015b72c890765b259196
Eric Wang <wjun0912@gmail.com>
2019-05-09 23:32:58 +1000

    Add test utils (#13)

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod sort;
 mod max_subarray;
 mod my_collections;
+mod test_utils;
 
 // TODO profile cases here instead of in the tests
 fn main() {}
---
+++ src/test_utils/test_case.rs
@@ -0,0 +1,33 @@
+pub struct TestCase<T, R> {
+  case: T,
+  answer: R,
+}
+
+impl<T, R> TestCase<T, R> {
+  fn new<F>(case: T, func: F) -> TestCase<T, R>
+    where F: Fn(&T) -> R {
+    let answer = func(&case);
+    TestCase { case, answer }
+  }
+}
+
+#[cfg(test)]
+mod test {
+  fn double(arr: &Vec<i32>) -> Vec<i32> {
+    let mut res: Vec<i32> = Vec::new();
+    arr.iter().for_each(|a| { res.push(*a * 2) });
+    res
+  }
+
+  #[test]
+  fn new() {
+    let test_case0: super::TestCase<i32, i32> = super::TestCase::new(1, |int| {
+      *int + 1
+    });
+    assert_eq!(test_case0.case, 1);
+    assert_eq!(test_case0.answer, 2);
+    let test_case1: super::TestCase<Vec<i32>, Vec<i32>> = super::TestCase::new(vec![1, 2, 3], double);
+    assert_eq!(test_case1.case, vec![1, 2, 3]);
+    assert_eq!(test_case1.answer, vec![2, 4, 6]);
+  }
+}
