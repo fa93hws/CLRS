864e5896583469e0dc85297b7a71436bd969eadc
Eric <eric@canva.com>
2019-05-09 23:32:38 +1000

    use fn for one test case

--- src/test_utils/test_case.rs
+++ src/test_utils/test_case.rs
@@ -13,6 +13,12 @@ impl<T, R> TestCase<T, R> {
 
 #[cfg(test)]
 mod test {
+  fn double(arr: &Vec<i32>) -> Vec<i32> {
+    let mut res: Vec<i32> = Vec::new();
+    arr.iter().for_each(|a| { res.push(*a * 2) });
+    res
+  }
+
   #[test]
   fn new() {
     let test_case0: super::TestCase<i32, i32> = super::TestCase::new(1, |int| {
@@ -20,11 +26,7 @@ mod test {
     });
     assert_eq!(test_case0.case, 1);
     assert_eq!(test_case0.answer, 2);
-    let test_case1: super::TestCase<Vec<i32>, Vec<i32>> = super::TestCase::new(vec![1, 2, 3], |arr| {
-      let mut res: Vec<i32> = Vec::new();
-      arr.iter().for_each(|a| { res.push(*a * 2) });
-      res
-    });
+    let test_case1: super::TestCase<Vec<i32>, Vec<i32>> = super::TestCase::new(vec![1, 2, 3], double);
     assert_eq!(test_case1.case, vec![1, 2, 3]);
     assert_eq!(test_case1.answer, vec![2, 4, 6]);
   }
