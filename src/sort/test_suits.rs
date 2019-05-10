3907dd5bc28b0497e5ab13a1e72e75772ad336d5
Eric <eric@canva.com>
2019-05-10 21:48:47 +1000

    golbal static test cases

--- src/sort/test_suits.rs
+++ src/sort/test_suits.rs
@@ -1,10 +1,13 @@
-use std::sync::Once;
 use crate::test_utils::creation::create_random_i32_vector;
 use crate::test_utils::test_case::TestCase;
 
 const CASES_SIZE: usize = 12;
-static mut CASES: [TestCase<Vec<i32>, Vec<i32>>; CASES_SIZE] = [];
-static INIT: Once = Once::new();
+
+lazy_static! {
+  static ref CASES: Vec<TestCase<Vec<i32>, Vec<i32>>> = {
+    create_test_cases()
+  };
+}
 
 fn create_test_problems() -> Vec<Vec<i32>> {
   let mut problems = vec![
@@ -18,21 +21,19 @@ fn create_test_problems() -> Vec<Vec<i32>> {
   problems
 }
 
-fn create_test_case(problem: Vec<i32>) {
-  let test_case = TestCase::new(problem, |p| {
+fn create_test_case(problem: &Vec<i32>) -> TestCase<Vec<i32>, Vec<i32>> {
+  TestCase::new(problem.clone(), |p| {
     let mut answer = p.clone();
     answer.sort();
     answer
-  });
-  unsafe { CASES.push(test_case); }
+  })
 }
 
-fn create_test_cases() {
+fn create_test_cases() -> Vec<TestCase<Vec<i32>, Vec<i32>>> {
   let problems = create_test_problems();
-  for problem in problems { create_test_case(problem); }
+  problems.iter().map(create_test_case).collect()
 }
 
 pub fn get_test_cases() -> &'static Vec<TestCase<Vec<i32>, Vec<i32>>> {
-  INIT.call_once(create_test_cases);
-  unsafe { &CASES }
+  &CASES
 }
