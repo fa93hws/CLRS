cc25b0f831c65ec16810673369e5c6f9e3526a94
Eric <eric@canva.com>
2019-05-10 22:08:54 +1000

    Use test util in max array

---
+++ src/max_subarray/test_suits.rs
@@ -0,0 +1,38 @@
+use crate::max_subarray::brute_force::find;
+use crate::test_utils::creation::create_random_i32_vector;
+use crate::test_utils::test_case::TestCase;
+
+const CASES_SIZE: usize = 12;
+
+lazy_static! {
+  static ref CASES: Vec<TestCase<Vec<i32>, i32>> = {
+    create_test_cases()
+  };
+}
+
+fn create_test_problems() -> Vec<Vec<i32>> {
+  let mut problems = vec![
+    vec![] as Vec<i32>,
+    vec![1]
+  ];
+  for _ in 0..(CASES_SIZE - problems.len()) {
+    let random_case = create_random_i32_vector(500, -100, 100);
+    problems.push(random_case);
+  }
+  problems
+}
+
+fn create_test_case(problem: &Vec<i32>) -> TestCase<Vec<i32>, i32> {
+  TestCase::new(problem.clone(), |p| {
+    find(p)
+  })
+}
+
+fn create_test_cases() -> Vec<TestCase<Vec<i32>, i32>> {
+  let problems = create_test_problems();
+  problems.iter().map(create_test_case).collect()
+}
+
+pub fn get_test_cases() -> &'static Vec<TestCase<Vec<i32>, i32>> {
+  &CASES
+}
